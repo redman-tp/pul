87ada289fbc2640a2fe951178df95292357b7a65
3m1n3nc3 <mygames.ng@gmail.com>
2025-02-02 16:17:49 +0100

    feat: Add visit.sh

--- visit.sh
+++ visit.sh
@@ -4,7 +4,7 @@ counter=0
 max_parallel=100 # Limit parallel requests
 
 while true; do
-  response_code=$(curl -s -o /dev/null -w "%{http_code}" "https://example.com" \
+  response_code=$(curl -s -o /dev/null -w "%{http_code}" "https://greysoft.ng" \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
     -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8" \
     -H "Accept-Language: en-US,en;q=0.9") & # Run in the background
